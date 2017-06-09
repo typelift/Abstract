0515615d5ecdc22acb26a8e06a6a639dc3fa5e5f
Elviro Rocca <elviro.rocca@facile.it>
2017-06-09 09:00:37 +0200

    some additions

--- Sources/Law.swift
+++ Sources/Law.swift
@@ -1,3 +1,11 @@
-/// the "Law" namespace
+/* 
+# Law
+	
+The "Law" namespace. 
 
-public enum Law {}
+Laws are basically equations that express a particular "invariant", that is, a rule that must be always fulfilled, or in other words, an equality that must be always true.
+
+Thus, for any law to be enforced for a particular type (the `Element` parameter), it's required that the type conforms to `Equatable`, so that an equation can be written and solved.
+*/
+
+public enum Law<Element: Equatable> {}
--- Sources/Magma.swift
+++ Sources/Magma.swift
@@ -1,5 +1,15 @@
-/// a Magma is a type with a composition operation; no particular rules are given
+/*
+# Magma
+
+A Magma is a type with a composition operation, or in other words, a "binay" operation, that is, an operation that takes two values of a particular type, and returns a value of the same type.
+
+It's a lot simpler and more readable to just define the `<>` (diamond) operator.
+
+The only law that's required is that the operation must be **closed**, that is, there cannot be preconditions that make the operation fail: the operation must be valid for **any** two values of the particular type.
+*/
+
+infix operator <> : AdditionPrecedence
 
 public protocol Magma {
-	func composed(_ other: Self) -> Self
+	static func <> (left: Self, right: Self) -> Self
 }
