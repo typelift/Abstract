351742d90260305905ece02f6564b68ed3fd348d
Elviro Rocca <elviro.rocca@facile.it>
2017-06-12 17:28:59 +0200

    New Predicate

--- Sources/Utilities/Predicate.swift
+++ Sources/Utilities/Predicate.swift
@@ -1,15 +1,23 @@
 /*:
 # Predicate
 
-A `Predicate` is a function from any type to `And`: it usually represent an assertion that some property is true or false for an instance of a particular type.
+A `Predicate` is a function from any type to `Bool`: it usually represent an assertion that some property is true or false for an instance of a particular type.
 
-Because `And` is a `BoundedSemilattice`, we can express a predicate with a typealias of `FunctionBS`, thus obtaining all the properties.
+Because `And` and 'Or' are `BoundedSemilattice`, we can express a predicate with a typealias of `FunctionBS`, thus obtaining all the properties.
 */
 
-public typealias Predicate<A> = FunctionBS<A,And>
+public typealias PredicateAnd<A> = FunctionBS<A,And>
 
 extension Sequence {
-	public func filtered(by predicate: Predicate<Iterator.Element>) -> [Iterator.Element] {
+	public func filtered(by predicate: PredicateAnd<Iterator.Element>) -> [Iterator.Element] {
+		return filter { predicate.call($0).value }
+	}
+}
+
+public typealias PredicateOr<A> = FunctionBS<A,Or>
+
+extension Sequence {
+	public func filtered(by predicate: PredicateOr<Iterator.Element>) -> [Iterator.Element] {
 		return filter { predicate.call($0).value }
 	}
 }
