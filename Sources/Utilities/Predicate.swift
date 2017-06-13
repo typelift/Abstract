2cf050cd1db363e70dbd4a5beb1bbb079a4bd784
Elviro Rocca <elviro.rocca@facile.it>
2017-06-13 08:31:58 +0200

    fixed a comment

--- Sources/Utilities/Predicate.swift
+++ Sources/Utilities/Predicate.swift
@@ -3,7 +3,7 @@
 
 A `Predicate` is a function from any type to `Bool`: it usually represent an assertion that some property is true or false for an instance of a particular type.
 
-Because `And` and 'Or' are `BoundedSemilattice`, we can express a predicate with a typealias of `FunctionBS`, thus obtaining all the properties.
+Because `And` and `Or` are `BoundedSemilattice`, we can express a predicate with a typealias of `FunctionBS`, thus obtaining all the properties.
 */
 
 public typealias PredicateAnd<A> = FunctionBS<A,And>
