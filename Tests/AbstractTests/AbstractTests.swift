edac2e1892af025d8fc277d940f627862c5d53dd
Elviro Rocca <elviro.rocca@icloud.com>
2017-07-16 21:39:48 +0200

    added Sourcery tests for all types

--- Tests/AbstractTests/AbstractTests.swift
+++ Tests/AbstractTests/AbstractTests.swift
@@ -3,32 +3,6 @@ import XCTest
 import SwiftCheck
 
 final class AbstractTests: XCTestCase {
-	func testSemiring() {
-		property("Bool is a Semiring: Distributive") <- forAll { (a: Bool, b: Bool, c: Bool) in
-			Law<Bool>.multiplicationIsDistributiveOverAddition(a, b, c)
-		}
-
-		property("Bool is a Semiring: Annihilation") <- forAll { (a: Bool) in
-			Law<Bool>.zeroAnnihiliatesTheMultiplicative(a)
-		}
-
-		property("FunctionSR is a Semiring: Distributive") <- forAll { (a: FunctionSROf<Int,TestSemiring>, b: FunctionSROf<Int,TestSemiring>, c: FunctionSROf<Int,TestSemiring>, context: Int) in
-			LawInContext<FunctionSR<Int,TestSemiring>>.multiplicationIsDistributiveOverAddition(a.get, b.get, c.get)(context)
-		}
-
-		property("FunctionSR is a Semiring: Annihilation") <- forAll { (a: FunctionSROf<Int,TestSemiring>, context: Int) in
-			LawInContext<FunctionSR<Int,TestSemiring>>.zeroAnnihiliatesTheMultiplicative(a.get)(context)
-		}
-        
-        property("Tropical is a Semiring: Distributive") <- forAll { (a: TropicalOf<Int>, b: TropicalOf<Int>, c: TropicalOf<Int>) in
-            Law<Tropical<Int>>.multiplicationIsDistributiveOverAddition(a.get, b.get, c.get)
-        }
-        
-        property("Tropical is a Semiring: Annihilation") <- forAll { (a: TropicalOf<Int>) in
-            Law<Tropical<Int>>.zeroAnnihiliatesTheMultiplicative(a.get)
-        }
-	}
-
 	func testHomomorphism() {
 		property("Ordering.reversed is a Homomorphism") <- forAll { (a: Ordering, b: Ordering) in
 			Law<Ordering>.isHomomorphism({ $0.reversed }, a, b)
