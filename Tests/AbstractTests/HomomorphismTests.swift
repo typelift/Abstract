01e655f439514edf2c62ca37559c46ef3a69e063
Elviro Rocca <elviro.rocca@icloud.com>
2017-07-16 22:13:35 +0200

    added HomomorphismTests; added LinuxMain.stencil template

--- Tests/AbstractTests/HomomorphismTests.swift
+++ Tests/AbstractTests/HomomorphismTests.swift
@@ -2,19 +2,22 @@ import XCTest
 @testable import Abstract
 import SwiftCheck
 
-final class AbstractTests: XCTestCase {
-	func testHomomorphism() {
+final class HomomorphismTests: XCTestCase {
+
+	func testOrderingReversed() {
 		property("Ordering.reversed is a Homomorphism") <- forAll { (a: Ordering, b: Ordering) in
 			Law<Ordering>.isHomomorphism({ $0.reversed }, a, b)
 		}
-		
+	}
+
+	func testComparisonReversed() {
 		property("Comparison.reversed is a Homomorphism") <- forAll { (a: FunctionMOf<TestProduct,Ordering>, b: FunctionMOf<TestProduct,Ordering>, context: TestProduct) in
 			LawInContext<Comparison<Int>>.isHomomorphism({ $0.reversed }, Comparison<Int> { a.get.call(TestProduct.init($0)) }, Comparison<Int> { b.get.call(TestProduct.init($0)) })(context.unwrap)
 		}
 	}
 	
 	static var allTests = [
-		("testSemiring",testSemiring),
-		("testHomomorphism",testHomomorphism)
+		("testOrderingReversed",testOrderingReversed),
+		("testComparisonReversed",testComparisonReversed)
 	]
 }
--- sourceryTests.sh
+++ sourceryTests.sh
@@ -6,6 +6,11 @@ if [ -f "Sourcery/sourcery" ]; then
 	echo "Sourcery: Sources/Abstract + Templates/Tests -> Tests/AbstractTests"
 
 	./Sourcery/sourcery --sources Sources/Abstract --templates Templates/Tests --output Tests/AbstractTests
+
+	echo "Sourcery: Tests/AbstractTests + Templates/Other/LinuxMain.stencil -> Tests/LinuxMain.swift"
+
+	./Sourcery/sourcery --sources Tests/AbstractTests --templates Templates/Other/LinuxMain.stencil --output Tests/LinuxMain.swift
+
 else
 	echo "Sourcery is not installed, ignoring."
 fi
